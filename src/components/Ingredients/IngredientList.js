4089c1f1bcf9a6306676f61c49515d8dbf813a99
Francois Polo <andasan@gmail.com>
2021-03-17 11:08:47 -0700

    useeffect check

--- src/components/Ingredients/IngredientList.js
+++ src/components/Ingredients/IngredientList.js
@@ -3,12 +3,13 @@ import React from 'react';
 import './IngredientList.css';
 
 const IngredientList = props => {
+  console.log(props)
   return (
     <section className="ingredient-list">
       <h2>Loaded Ingredients</h2>
       <ul>
         {props.ingredients.map(ig => (
-          <li key={ig.id} onClick={props.onRemoveItem.bind(this, ig.id)}>
+          <li key={ig.id} onClick={() => props.onRemoveItem(ig.id)}>
             <span>{ig.title}</span>
             <span>{ig.amount}x</span>
           </li>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,19 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList'
 import Search from './Search';
 
 function Ingredients() {
-
   const [userIngredients, setUserIngredients] = useState([])
 
+  useEffect(() => {
+    fetch('https://react-hooks-update-caccd-default-rtdb.firebaseio.com/ingredients.json')
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = []
+        for (const key in responseData){
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount
+          })
+        }
+        setUserIngredients(loadedIngredients)
+      })
+  }, [])
+
+  useEffect(() => {
+    console.log('Rendering Ingredients... ', userIngredients)
+    // return () => {
+    //   console.log('This component will be unmounted.....')
+    // }
+  }, [userIngredients])
+
   const addIngredientHandler = (ingredients) => {
-    setUserIngredients((prevState) => [
-      ...prevState,
-      { id: Math.random().toString(), ...ingredients}
-    ])
+    // setUserIngredients((prevState) => [
+    //   ...prevState,
+    //   { id: Math.random().toString(), ...ingredients}
+    // ])
+    fetch('https://react-hooks-update-caccd-default-rtdb.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredients),
+      headers: { 'Content-Type': 'application/json'}
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        console.log(responseData)
+        setUserIngredients((prevState) => [
+            ...prevState,
+            { id: responseData.name, ...ingredients}
+          ])
+      })
   } 
+
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevState => {
+      prevState.filter(ingredient =>  ingredient.id !== ingredientId)
+    })
+  }
 
 
   return (
