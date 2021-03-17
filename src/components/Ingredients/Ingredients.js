afc85870827bdeaac00f370b56843adc291727d3
Francois Polo <andasan@gmail.com>
2021-03-17 10:04:35 -0700

    usestate cleared

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList'
 import Search from './Search';
 
 function Ingredients() {
+
+  const [userIngredients, setUserIngredients] = useState([])
+
+  const addIngredientHandler = (ingredients) => {
+    setUserIngredients((prevState) => [
+      ...prevState,
+      { id: Math.random().toString(), ...ingredients}
+    ])
+  } 
+
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
