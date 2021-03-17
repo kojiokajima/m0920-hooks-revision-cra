410d3a27240cad4f900aafd90cd1c1c8ff33521a
wanko <kouji124@w4.dion.ne.jp>
2021-03-17 12:06:07 -0700

    done

--- src/components/WeatherApp/CityWeather.js
+++ src/components/WeatherApp/CityWeather.js
@@ -1,34 +1,30 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 
-class CityWeather extends Component {
+const CityWeather = (props) => {
 
 
-    state = {}
+    const [icon, seticon] = useState("")
 
-    async componentDidMount(){
-        this.getWeather();
-    }
+    // useEffect( () => {
+    //     getWeather();
+    // }, [])
 
-    componentDidUpdate(oldProps){
-        if(oldProps.cityName !== this.props.cityName){
-            this.getWeather();
-        }
-    }
+    useEffect(() => {
+            getWeather();
+    }, [props.cityName])
 
-    getWeather = async()=>{
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.props.cityName}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+    const getWeather = async()=>{
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${props.cityName}&units=metric&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
         const resp = await axios.get(url);
         console.log(resp.data);
     }
 
-    render(){
-        const iconUrl = `http://openweathermap.org/img/w/${this.state.icon}.png`
+    const iconUrl = `http://openweathermap.org/img/w/${icon}.png`
 
-        return(
-            <h1>{this.props.cityName}</h1>
-        )
-    }
+    return(
+        <h1>{props.cityName}</h1>
+    )
 }
 
 export default CityWeather;
--- src/components/WeatherApp/WeatherApp.js
+++ src/components/WeatherApp/WeatherApp.js
@@ -1,34 +1,30 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import CityWeather from './CityWeather';
 
-class WeatherApp extends Component {
-    state = {
-        city: "London",
-        cityNameForWeather: "London",
+const WeatherApp = () => {
+    const [city, setCity] = useState("London")
+    const [cityNameForWeather, setCityNameForWeather] = useState("London")
+
+    const changeCity = (e)=>{
+        setCity(e.target.value)
     }
 
-    changeCity = (e)=>{
-        this.setState({city:e.target.value})
-    }
-
-    citysearch = (e)=>{
+    const citysearch = (e)=>{
         e.preventDefault();
-        this.setState({cityNameForWeather: this.state.city})
+        setCityNameForWeather(city)
     }
 
-    render(){
-        return(
-            <div className="container">
-                <CityWeather cityName={this.state.cityNameForWeather} />
-                <div className="row justify-content-center">
-                    <form onSubmit={this.citysearch}>
-                        <input type="text" value={this.state.city} onChange={this.changeCity} />
-                        <input type="submit" className="btn btn-primary" value="Search!" />
-                    </form>
-                </div>
+    return(
+        <div className="container">
+            <CityWeather cityName={cityNameForWeather} />
+            <div className="row justify-content-center">
+                <form onSubmit={citysearch}>
+                    <input type="text" value={city} onChange={changeCity} />
+                    <input type="submit" className="btn btn-primary" value="Search!" />
+                </form>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default WeatherApp;
